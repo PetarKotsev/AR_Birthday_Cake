a684ad8af281c2c241e3c351b5ca4d761c9f56ff
Petar Kocev <petarkocew@gmail.com>
2019-08-04 18:56:39 +0300

    initial

---
+++ Assets/vbScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+[RequireComponent(typeof(AudioSource))]
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
+ 
+
+	public GameObject vb_GO;
+	public AudioSource source;
+	public AudioClip clip;
+	public bool musicPlayed = false;
+
+	// Use this for initialization
+
+	void Start () {
+
+		vb_GO = GameObject.Find ("playButton");
+		vb_GO.GetComponent<VirtualButtonAbstractBehaviour>().RegisterEventHandler(this);
+		//source = AudioSource.Find ("Music");
+		source = GetComponent<AudioSource> ();
+	}
+	
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb)
+	{
+		Debug.Log ("Button down");
+		if (!musicPlayed) {
+			source.Play ();
+			musicPlayed = true;
+		}
+	}
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb)
+	{
+		
+	}
+}
